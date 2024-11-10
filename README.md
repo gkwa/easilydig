6503cb594a35a80c8b831acf6fb9c0974965836c
Taylor Monacelli <taylormonacelli@gmail.com>
2024-11-09 22:58:27 -0800

    Boilerplate

---
+++ README.md
@@ -0,0 +1,30 @@
+# easilydig
+
+Purpose:
+
+
+## example usage
+
+```bash
+
+
+```
+
+## install easilydig
+
+
+on macos/linux:
+```bash
+
+brew install gkwa/homebrew-tools/easilydig
+
+```
+
+
+on windows:
+
+```powershell
+
+TBD
+
+```
---
+++ cmd/version.go
@@ -0,0 +1,23 @@
+package cmd
+
+import (
+	"fmt"
+	
+	"github.com/spf13/cobra"
+
+	"github.com/gkwa/easilydig/version"
+)
+
+var versionCmd = &cobra.Command{
+	Use:   "version",
+	Short: "Print the version number of easilydig",
+	Long:  `All software has versions. This is easilydig's`,
+	Run: func(cmd *cobra.Command, args []string) {
+		buildInfo := version.GetBuildInfo()
+		fmt.Println(buildInfo)
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(versionCmd)
+}
